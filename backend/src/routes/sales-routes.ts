798a6150bc3e2f99a13631c374a31009bb1e324e
FvCastr0 <fvdc2008@gmail.com>
2025-01-23 11:03:51 -0300

    sales controller

--- backend/src/routes/sales-routes.ts
+++ backend/src/routes/sales-routes.ts
@@ -1,7 +1,12 @@
+import { FindSalesMonthController } from "@controllers/sales/find-sales-month-controller";
 import { MakeSaleController } from "@controllers/sales/make-sale-controller";
 import Express, { Router } from "express";
 
 const router = Router();
+
+router.get("/:id", async (req: Express.Request, res: Express.Response) => {
+  await new FindSalesMonthController().execute(req, res);
+});
 
 router.post("/new", async (req: Express.Request, res: Express.Response) => {
   await new MakeSaleController().execute(req, res);
